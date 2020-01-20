446b39e86f088ab5fe2bf2fb6e64cf9a0be51db5
Stéphane Depierrepont <stephane@dpp.st>
2020-01-20 08:13:24 +0100

    init config

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"kinsta/services/config"
+	"os"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	// home
+	home, err := os.Executable()
+	if err != nil {
+		fmt.Printf("os.Executable() failed - %v", err)
+		os.Exit(1)
+	}
+
+	// config
+	if err = config.Init(home); err != nil {
+		fmt.Printf("config.Init(%s) failed - %v", home, err)
+	}
+
 	e := echo.New()
 
 	// recover
---
+++ services/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"path"
+
+	"github.com/spf13/viper"
+)
+
+func Init(home string) error {
+	viper.SetConfigFile(path.Join(home, "config.yaml"))
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	viper.Set("home", home)
+	viper.WatchConfig()
+	return nil
+}
